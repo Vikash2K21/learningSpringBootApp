9d4765b597e4cbb564e40c15bb73bc0720a17038
Vikash Kumar <vikashmrh1@gmail.com>
2025-11-20 18:53:55 +0530

    Initial commit

---
+++ learningSpringBootApp/src/main/java/com/codingshuttle/youtube/learningSpringBootApp/LearningSpringBootAppApplication.java
@@ -0,0 +1,14 @@
+package com.codingshuttle.youtube.learningSpringBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringBootAppApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(LearningSpringBootAppApplication.class, args);
+	}
+
+}
